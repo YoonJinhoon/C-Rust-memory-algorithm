8d36c77f9ea16073a9801ff920f986bfa7535f92
YoonJinhoon <https://github.com/YoonJinhoon>
2026-08-31 15:37:44 +0900

    feat: implement safe stack in Rust

---
+++ rust/01_stack/stack_project/src/main.rs
@@ -0,0 +1,48 @@
+pub struct Stack {
+    data: [i32; 5],
+    top: isize,
+}
+
+impl Stack {
+    pub fn new() -> Self {
+        Stack {
+            data: [0; 5],
+            top: -1,
+        }
+    }
+
+    pub fn push(&mut self, value: i32) {
+        if self.top >= 4 {
+            println!("Stack Overflow!");
+            return;
+        }
+        self.top += 1;
+        self.data[self.top as usize] = value;
+        println!("Pushed {} -> Index: {}", value, self.top);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        if self.top < 0 {
+            println!("Stack Underflow!");
+            None
+        } else {
+            let val = self.data[self.top as usize];
+            println!("Popped {} <- index: {}", val, self.top);
+            self.top -= 1;
+            Some(val)
+        }
+    }
+}
+
+fn main() {
+    let mut s = Stack::new();
+    
+    println!("--- Pushing to Stack ---");
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    
+    println!("\n--- Popping from Stack ---");
+    s.pop();
+    s.pop();
+}    
